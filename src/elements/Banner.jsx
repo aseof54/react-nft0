93ec5fed1e46eef161343e1e2667116bb16759f0
display-inline-flex <mikjarkov2@gmail.com>
2025-04-12 11:15:09 +0300

    Feature: Banner Raw

--- src/elements/Banner.jsx
+++ src/elements/Banner.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import Button from "../components/Button";
-
+import bestNftImg from "../assets/images/banners/bestNft.png";
 const Banner = () => {
+  const statistics = [
+    {
+      value: "32k+",
+      title: "Artworks",
+    },
+    {
+      value: "32k+",
+      title: "Auctions",
+    },
+    {
+      value: "12k+",
+      title: "Creators",
+    },
+  ];
+  const bestNft = {
+    img: { bestNftImg },
+    price: "0.99 ETH",
+    endsIn: "25 hrs",
+    artist: "@wzard",
+  };
   return (
     <section className="banner">
       <div className="left-part">
@@ -28,9 +48,50 @@ const Banner = () => {
             Create
           </Button>
         </div>
-        <div className="numbers"></div>
+        <div className="numbers">
+          {statistics.map((element, key) => (
+            <div key={key} className="number">
+              <h3>{element.value}</h3>
+              <span>{element.title}</span>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="right-part"></div>
+      <div className="right-part">
+        <div
+          className="card"
+          style={{ backgroundImage: `url(${bestNft.img})` }}
+        >
+          <div className="top-content">
+
+            <div className="nft-info">
+              <span>Current bid</span>
+              <h3>{bestNft.price}</h3>
+            </div>
+            <div className="nft-info">
+              <span>Ends in</span>
+              <h3>{bestNft.endsIn}</h3>
+            </div>
+
+          </div>
+
+          <div className="bottom-content">
+
+            <div className="artist">
+              <span>Artist</span>
+              <span>{bestNft.artist}</span>
+            </div>
+            <Button
+              backgroundColor="#FFCE4E"
+              padding="23px 44px"
+              textColor="#000"
+            >
+              Start Bid
+            </Button>
+            
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
